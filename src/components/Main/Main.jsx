b408e3a7eafe04f58624b5a5e7e4c27eec27f5b0
yam <yam@Yam-to.local>
2022-06-26 14:30:45 +0300

    DelpoyCondole

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,20 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css"
 import Card from "./Card"
 
 const Main = () => {
+    const [catalog, setCatalog] = useState(null)
+    useEffect(() => {
+        const el = document.getElementById("data")
+        const data = JSON.parse(el.getAttribute("data"))
+        console.log("data")
+        console.log(el, data)
+        setCatalog(data)
+    })
     return (
         <main className="main">
-            <Card />
+            <Card catalog={catalog} />
         </main>
     )
 }
