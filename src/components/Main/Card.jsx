3944ecd4f235e31baa8b822e925087b7752cb191
yam <yam@Yam-to.local>
2022-06-26 15:00:54 +0300

    DelpoyConsole

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -7,11 +7,12 @@ const Card = (props) => {
     if (!props.catalog) {
         return null
     }
+    console.log(props.catalog.hz_building_area_min)
     return (
         <>
             <div className="item__img-wrap">
-                <a className="item__link" href={props.url}>
-                    <img className="item__img" src={props.thumb} alt="" />
+                <a className="item__link" href={props.catalog.url}>
+                    <img className="item__img" src={props.catalog.thumb} alt="" />
                 </a>
                 <div className="item__photo"><img className="item__photo-icon" src="" alt="" />
                     <div className="item__photo-count"></div>
@@ -19,13 +20,13 @@ const Card = (props) => {
             </div>
             <div className="item__container">
                 <div className="item__row"><img className="item__icon" src="" alt="" />
-                    <div className="item__text">{props.hz_roads}</div>
+                    <div className="item__text">{props.catalog.hz_roads}</div>
                 </div>
-                <a className="item__name" href="url">{props.hz_building_name_obj}</a>
+                <a className="item__name" href="url">{props.catalog.hz_building_name_obj}</a>
                 <div className=" item__row"><img className="item__icon" src="./images/area.png" alt="" />
-                    <div className="item__text">{props.hz_building_area_min} до {props.hz_building_area_max} м2</div>
+                    <div className="item__text">{props.catalog.hz_building_area_min} до {props.catalog.hz_building_area_max} м2</div>
                 </div>
-                <div className="item__text">{props.hz_temp_price}</div>
+                <div className="item__text">{props.catalog.hz_temp_price}</div>
             </div >
     </>
     )
